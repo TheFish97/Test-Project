f01979aed9e2bcac6c96e8b119be50d1bc51a7b0
AashrayAnand <aashray_anand@hotmail.com>
2015-08-13 22:10:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-@@ -0,0 +1,29 @@
-
 /*jslint latedef:false*/
 document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";
-var x = parseInt(prompt("How long is the first array", "1"), 10)
-var y = parseInt(prompt("How long is the second array", "1"), 10)
+var x = parseInt(prompt("How long is the first array", "1"), 10);
+var y = parseInt(prompt("How long is the second array", "1"), 10);
 var array = new Array(x)
 var array2 = new Array(y)
-var array3 = new Array(x+y)
-var a = array.length
-var b = array2.length
-document.write("The length of the first array is " + a )
-document.write(" The length of the second array is " + b)
-for(var i = 0; i < a; i++){
-    array[i] = parseInt(prompt("Enter a number", "1"), 10)
+var a = array.length;
+var b = array2.length;
+var c = b + a;
+document.write("Combined length:  " + c);
+for(var i = 0; i < array.length; i++){
+    array[i] = parseInt(prompt("Fill the first array", "1"), 10);
 }
-for(var j = 0; i < b; i++){
-    array[j] = parseInt(prompt("Enter a number", "1"), 10)
+for(var j = 0; j < array2.length; j++){
+    array2[j] = parseInt(prompt("Fill the second array ", "1"), 10);
 }
-var z = 0
-var q = 0
-var l = 0
-while(z < a-1 && q < b-1){
-    if(array[z]<array2[q]){
-        array3[l] = array[z]
+document.write(" [" + array + "]");
+document.write(" [" + array2 + "]");
+document.write(".......Final Array ->");
+var m = 0;
+var k = 0;
+var l = 0;
+var array3 = new Array(c);
+while(m<array.length && k<array2.length){
+    if(array[m] < array2[k]){
+        array3[l] = array[m];
+        m = m + 1;
+    }else{
+        array3[l] = array2[k];
+        k = k + 1;
     }
-    else{
-        array3[l] = array2[q]
-    }
+    l = l + 1;
 }
-document.write("                     " + array3)
+if(m<k){
+    while (m < array.length)
+    {
+        array3[l] = array[m];
+        l = l + 1;
+        m = m + 1;
+    }
+}else{
+    while (k < array2.length)
+    {
+        array3[l] = array[k];
+        l = l + 1;
+        k = k + 1;
+    }
+    
+}
+document.write(array3);
+
