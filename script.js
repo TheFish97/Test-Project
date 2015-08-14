836cbbbb4c42f56a63f788cb906eb8feea786240
AashrayAnand <aashray_anand@hotmail.com>
2015-08-13 22:22:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*jslint latedef:false*/
 document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";
-var x = parseInt(prompt("How long is the first array", "1"), 10);
-var y = parseInt(prompt("How long is the second array", "1"), 10);
+var x = parseInt(prompt("What is the length of the first array", "1"), 10);
+var y = parseInt(prompt("What is the length of the second array", "1"), 10);
 var array = new Array(x)
 var array2 = new Array(y)
 var a = array.length;
@@ -9,10 +9,10 @@ var b = array2.length;
 var c = b + a;
 document.write("Combined length:  " + c);
 for(var i = 0; i < array.length; i++){
-    array[i] = parseInt(prompt("Fill the first array", "1"), 10);
+    array[i] = parseInt(prompt("What is the value at index " + i + " of the first array", "1"), 10);
 }
 for(var j = 0; j < array2.length; j++){
-    array2[j] = parseInt(prompt("Fill the second array ", "1"), 10);
+    array2[j] = parseInt(prompt("What is the value at index " + i + " of the second array", "1"), 10);
 }
 document.write(" [" + array + "]");
 document.write(" [" + array2 + "]");
