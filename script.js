0d585e241eb8369de7e8e1407106508affef1d84
AashrayAnand <aashray_anand@hotmail.com>
2015-08-13 19:57:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*jslint latedef:false*/
-document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";
+/*document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";*/
 var x = parseInt(prompt("How long is the first array", "1"), 10);
 var y = parseInt(prompt("How long is the second array", "1"), 10);
 var array = new Array(x)
@@ -21,9 +21,26 @@ var m = 0;
 var k = 0;
 var l = m + k;
 var array3 = new Array(c)
-/*while((m<a) && (k<b)){
+while((m<a) && (k<b)){
     if(array[m] < array2[k]){
-        array3[l]
+        array3[l] = array[m];
+        m++;
+        l++;
+    }else{
+        array3[l] = array2[k];
+        k++;
+        l++;
     }
-    
-}*/
+}
+while(k<b){
+        array[l] = array2[k];
+        k++;
+        l++;
+    }
+while(m<a){
+    array[l] = array[m];
+    m++;
+    l++;
+    }
+}
+document.write(array3);
