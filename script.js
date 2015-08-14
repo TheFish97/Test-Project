b2bb3d3cd61f32a97312ac864093526babc98562
AashrayAnand <aashray_anand@hotmail.com>
2015-08-13 19:59:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*jslint latedef:false*/
-/*document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";*/
+document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";
 var x = parseInt(prompt("How long is the first array", "1"), 10);
 var y = parseInt(prompt("How long is the second array", "1"), 10);
 var array = new Array(x)
@@ -16,11 +16,11 @@ for(var j = 0; j < array2.length; j++){
 }
 document.write(" [ " + array);
 document.write("," + array2 + " ]");
-document.write(".......Final Array ->")
+document.write(".......Final Array ->");
 var m = 0;
 var k = 0;
 var l = m + k;
-var array3 = new Array(c)
+var array3 = new Array(c);
 /*while((m<a) && (k<b)){
     if(array[m] < array2[k]){
         array3[l] = array[m];
