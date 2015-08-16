b03a121ad70c77dcdf38d4c5ba12e68b446d7dfd
AashrayAnand <aashray_anand@hotmail.com>
2015-08-16 15:58:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-/*jslint latedef:false*/
 document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + "  All rights reserved </p>";
-var x = parseInt(prompt("What is the length of the first array", "1"), 10);
-var y = parseInt(prompt("What is the length of the second array", "1"), 10);
-var array = new Array(x)
-var array2 = new Array(y)
-var a = array.length;
-var b = array2.length;
-var c = b + a;
-document.write("Combined length:  " + c);
-for(var i = 0; i < array.length; i++){
-    array[i] = parseInt(prompt("What is the value at index " + i + " of the first array", "1"), 10);
+
+// Gather input
+function parseArrayInput(text) {
+
+    var parts = text.split(',');
+
+    var array = new Array(parts.length);
+
+    for (var i = 0; i < parts.length; i++) {
+        array[i] = Number(parts[i]);
+    }
+
+    return array;
 }
-for(var j = 0; j < array2.length; j++){
-    array2[j] = parseInt(prompt("What is the value at index " + j + " of the second array", "1"), 10);
-}
-document.write(" [" + array + "]");
-document.write(" [" + array2 + "]");
-document.write(".......Final Array ->");
-var m = 0;
-var k = 0;
-var l = 0;
-var array3 = new Array(c);
-while(m<array.length || k<array2.length){
-    if(array[m] < array2[k]){
+
+function runSort() {
+
+    var array = parseArrayInput(document.getElementById("array1").value);
+    var array2 = parseArrayInput(document.getElementById("array2").value);
+
+    // Algorithm
+    var array3 = new Array(array.length + array2.length);
+
+    var m = 0;
+    var k = 0;
+    var l = 0;
+    while (m < array.length && k < array2.length) {
+        if (array[m] < array2[k]) {
+            array3[l] = array[m];
+            m = m + 1;
+        } else {
+            array3[l] = array2[k];
+            k = k + 1;
+        }
+        l = l + 1;
+    }
+
+    // G: You don't need the extra if conditions as the while test takes care of each array's extras
+    while (m < array.length) {
         array3[l] = array[m];
+        l = l + 1;
         m = m + 1;
-    }else{
+    }
+
+    while (k < array2.length) {
+        // G: This had a bug where it was indexing into array rather than array2
         array3[l] = array2[k];
+        l = l + 1;
         k = k + 1;
     }
-    l = l + 1;
-}
-document.write("[" + array3 + "]");
 
+    // G: Rather than use d.ocument.write(), I set the innherHTML of an element by ID
+    // Write out the result
+    var resultMessage = "[" + array3 + "]";
+
+    document.getElementById("results").innerHTML = resultMessage;
+
+}
